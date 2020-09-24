64272961ed0e46b17864ade942f8df5faf48e482
Sean Delaney <seanmartindelaney@gmail.com>
2020-09-24 21:11:58 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require 'pry'
+
 def game_hash
   {
     home: {
@@ -126,4 +128,105 @@ def game_hash
   }
 end
 
-# Write code here
+def home_team_players
+  game_hash[:home][:players]
+  #binding.pry
+end
+
+def away_team_players
+  game_hash[:away][:players]
+end
+
+def num_points_scored(player)
+  variable = nil 
+  home_team_players.find do |a|
+    if a[:player_name] == player
+      variable = a[:points]
+    end
+  end
+  away_team_players.find do |a|
+    if a[:player_name] == player
+      variable = a[:points]
+    end
+  end
+  return variable 
+end
+
+def shoe_size(player)
+  variable = nil
+  home_team_players.find do |a|
+    if a[:player_name] == player
+      variable = a[:shoe]
+    end
+  end
+  away_team_players.find do |a|
+    if a[:player_name] == player
+      variable = a[:shoe]
+    end
+  end
+  return variable
+end
+
+def team_colors(team)
+  variable = nil 
+  if team == game_hash[:home][:team_name]
+    variable = game_hash[:home][:colors]
+  elsif team == game_hash[:away][:team_name]
+    variable = game_hash[:away][:colors]
+  end
+  return variable
+end
+
+def team_names
+  teams = game_hash[:home][:team_name], game_hash[:away][:team_name]
+  return teams
+end
+
+def player_numbers(team)
+  variable = [] 
+  if team == game_hash[:home][:team_name]
+    home_team_players.map do |a|
+      variable << a[:number]
+    end
+  elsif team == game_hash[:away][:team_name]
+    away_team_players.map do |a|
+      variable << a[:number]
+    end
+  end
+  return variable 
+end
+
+def player_stats(player)
+  variable = nil 
+  home_team_players.find do |a|
+    if a[:player_name] == player
+      variable = a
+    end
+  end
+  away_team_players.find do |a|
+    if a[:player_name] == player
+      variable = a 
+    end
+  end
+  return variable
+end
+
+def big_shoe_rebounds
+  player_variable = ""
+  shoe_variable = 0
+  game_hash.each do |home, b|
+    b[:players].each do |stats|
+      #binding.pry 
+      stats.each do |stat, value|
+        if stat == :shoe 
+          if value > shoe_variable
+            shoe_variable = value
+            player_variable = stats[:rebounds]
+          end
+        end
+      end
+    end
+  end
+  return player_variable 
+end
+  
